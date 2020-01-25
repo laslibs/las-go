b5bf6dd0c5b0ed5f122de3985b024889501bfa21
Ikechukwu <iykekings36@gmail.com>
2020-01-25 13:39:39 +0100

    add remove comment

--- las.go
+++ las.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strings"
 )
 
 //LasType is the main type definition
@@ -19,6 +20,18 @@ func Las(path string) (*LasType, error) {
 	return &l, err
 }
 
+func removeComment(str string) string {
+	trimmedStr := strings.Trim(str, " ")
+	strVec := strings.Split(trimmedStr, "\n")
+	var result strings.Builder
+	for _, line := range strVec {
+		strTrimLeft := strings.TrimLeft(line, " ")
+		if strTrimLeft[0] != '#' {
+			result.WriteString(fmt.Sprintf("%s\n", strTrimLeft))
+		}
+	}
+	return result.String()
+}
+
 func main() {
-	fmt.Println(Las("sample/example1.las"))
 }
