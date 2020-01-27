dec8fb9bd4ebf55a85d876596b2ddae3624bd954
Ikechukwu <iykekings36@gmail.com>
2020-01-27 09:24:24 +0100

    feat: add version, wrap, row and column count

--- las.go
+++ las.go
@@ -47,14 +47,15 @@ func chunk(s []string, n int) (store [][]string) {
 
 //Header - returns the header in the las file
 func (l *LasType) Header() ([]string, error) {
+	err := "file has no header content, make sure to create an instance of LasType with Las function"
 	if len(l.content) < 1 {
-		return []string{}, fmt.Errorf("file has no header content, make sure to create an instance of LasType with Las function")
+		return []string{}, fmt.Errorf(err)
 	}
 	hP := regexp.MustCompile("~C(?:\\w*\\s*)*\n\\s*")
 	spl := strings.Split(hP.Split(l.content, 2)[1], "~")[0]
 	headers := removeComment(spl)
 	if len(headers) < 1 {
-		return []string{}, fmt.Errorf("file has no header content, make sure to create an instance of LasType with Las function")
+		return []string{}, fmt.Errorf(err)
 	}
 	for i, val := range headers {
 		headers[i] = regexp.MustCompile("\\s+[.]").Split(strings.TrimSpace(val), 2)[0]
@@ -73,34 +74,54 @@ func (l *LasType) Data() [][]string {
 	return chunk(sBs, len(hds))
 }
 
-// func metadata(str string) {
-// 	// const str = await this.blobString;
-// 	// const sB = (str as string)
-// 	// 	.trim()
-// 	// 	.split(/~V(?:\w*\s*)*\n\s*/)[1]
-// 	// 	.split(/~/)[0];
-// 	sB := strings.Split(regexp.MustCompile("~V(?:\\w*\\s*)*\n\\s*").Split(str, 2)[1], "~")[0]
-// 	// const sw = Las.removeComment(sB);
-// 	sw := removeComment(sB)
-// 	// const refined = sw
-// 	// 	.split('\n')
-// 	// 	.map(m => m.split(/\s{2,}|\s*:/).slice(0, 2))
-// 	// 	.filter(f => Boolean(f));
-// 	for _, val := range sw {
+// Version - returns the version of the las file
+func (l *LasType) Version() (version string) {
+	version, _ = metadata(l.content)
+	return
+}
 
-// 	}
-// 	// const res = refined.map(r => r[1]);
-// 	// const wrap = res[1].toLowerCase() === 'yes' ? true : false;
-// 	// if ([+res[0], wrap].length < 0) {
-// 	// 	throw new LasError("Couldn't get metadata");
-// 	// }
-// 	// return [+res[0], wrap];
-// }
+// Wrap - returns the version of the las file
+func (l *LasType) Wrap() (wrap bool) {
+	_, wrap = metadata(l.content)
+	return
+}
+
+// ColumnCount - Returns the number of columns in a .las file
+func (l *LasType) ColumnCount() (count int) {
+	count = len(l.Data())
+	return
+}
+
+// RowCount - Returns the number of rowa in a .las file
+func (l *LasType) RowCount() (count int) {
+	header, _ := l.Header()
+	// TODO: handle error
+	count = len(header)
+	return
+}
+
+// metadata - picks out version and wrap state of the file
+func metadata(str string) (version string, wrap bool) {
+	sB := strings.Split(regexp.MustCompile("~V(?:\\w*\\s*)*\n\\s*").Split(str, 2)[1], "~")[0]
+	sw := removeComment(sB)
+	accum := [][]string{}
+	for _, val := range sw {
+		current := regexp.MustCompile("\\s{2,}|\\s*:").Split(val, -1)[0:2]
+		accum = append(accum, current)
+	}
+	version = accum[0][1]
+	if strings.ToLower(accum[1][1]) == "yes" {
+		wrap = true
+	} else {
+		wrap = false
+	}
+	return
+}
 
 func main() {
 	las, err := Las("sample/example1.las")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(las.Data())
+	fmt.Println(metadata(las.content))
 }
