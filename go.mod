4b5e3bd829321e5dd4dd8585ada23458eed318af
Samuel Ameh <propersam2012@gmail.com>
2020-02-01 15:57:46 +0100

    Add DataStruct method to LasGo that converts data to Struct and returns []interface

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/iykekings/las-go
 
 go 1.13
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/google/go-cmp v0.4.0
+	github.com/mitchellh/mapstructure v1.1.2
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543
+)
--- las.go
+++ las.go
@@ -1,11 +1,10 @@
 package lasgo
 
 import (
-	"errors"
+	"context"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"regexp"
 	"strings"
 )
 
@@ -15,50 +14,12 @@ type LasType struct {
 	content string
 }
 
-// index returns the position of an element in a slice of strings
-func index(slice []string, item string) int {
-	for i := range slice {
-		if slice[i] == item {
-			return i
-		}
-	}
-	return -1
-}
-
 //Las creates an instance of LasType
 func Las(path string) (*LasType, error) {
 	bs, err := ioutil.ReadFile(path)
 	l := LasType{path: path}
 	l.content = string(bs)
 	return &l, err
-}
-
-func removeComment(str string) []string {
-	trimmedStr := strings.TrimSpace(str)
-	strVec := strings.Split(trimmedStr, "\n")
-	var result []string
-	for _, line := range strVec {
-		fStr := strings.TrimSpace(line)
-		if !strings.HasPrefix(fStr, "#") && len(fStr) > 0 {
-			result = append(result, fStr)
-		}
-	}
-	return result
-}
-
-func pattern(str string) *regexp.Regexp {
-	return regexp.MustCompile(str)
-}
-
-func chunk(s []string, n int) (store [][]string) {
-	for i := 0; i < len(s); i += n {
-		if i+n >= len(s) {
-			store = append(store, s[i:])
-		} else {
-			store = append(store, s[i:i+n])
-		}
-	}
-	return
 }
 
 // WellProps contains basic definition of a single well measurement
@@ -95,6 +56,41 @@ func (l *LasType) Data() [][]string {
 	sB := pattern("~A(?:\\w*\\s*)*\n").Split(l.content, 2)[1]
 	sBs := pattern("\\s+").Split(strings.TrimSpace(sB), -1)
 	return chunk(sBs, len(hds))
+}
+
+// DataStruct just like Data but returns output in specified struct format
+func (l *LasType) DataStruct(opt *DataOptions) []interface{} {
+
+	hds, err := l.Header()
+	if err != nil {
+		panic("No data in file")
+	}
+	sB := pattern("~A(?:\\w*\\s*)*\n").Split(l.content, 2)[1]
+	sBs := pattern("\\s+").Split(strings.TrimSpace(sB), -1)
+
+	var (
+		o     *DataOptions
+		store [][]string
+	)
+
+	if opt != nil {
+		o = opt
+	}
+
+	store = chunk(sBs, len(hds))
+
+	if o != nil {
+		ctx := context.Background()
+
+		output, err := structConvert(ctx, store, hds, o)
+		if err != nil {
+			panic(err)
+		}
+
+		return output
+	}
+
+	return nil
 }
 
 // Version - returns the version of the las file
@@ -198,64 +194,4 @@ func (l *LasType) ToCSV(filename string) {
 		// TODO: don't include \n at the last line
 		file.WriteString(strings.Join(val, ",") + "\n")
 	}
-}
-
-// metadata - picks out version and wrap state of the file
-func metadata(str string) (version string, wrap bool) {
-	sB := strings.Split(pattern("~V(?:\\w*\\s*)*\n\\s*").Split(str, 2)[1], "~")[0]
-	sw := removeComment(sB)
-	accum := [][]string{}
-	for _, val := range sw {
-		current := pattern("\\s{2,}|\\s*:").Split(val, -1)[0:2]
-		accum = append(accum, current)
-	}
-	version = accum[0][1]
-	if strings.ToLower(accum[1][1]) == "yes" {
-		wrap = true
-	} else {
-		wrap = false
-	}
-	return
-}
-
-func property(str string, key string) (property map[string]WellProps, err error) {
-	err = errors.New("property cannot be found")
-	property = make(map[string]WellProps)
-
-	regDict := map[string]string{
-		"curve": "~C(?:\\w*\\s*)*\\n\\s*",
-		"param": "~P(?:\\w*\\s*)*\\n\\s*",
-		"well":  "~W(?:\\w*\\s*)*\\n\\s*",
-	}
-	prop, ok := regDict[key]
-	if !ok {
-		return
-	}
-	substr := pattern(prop).Split(str, 2)
-	var sw []string
-	if len(substr) > 1 {
-		sw = removeComment(strings.Split(substr[1], "~")[0])
-	}
-	if len(sw) > 0 {
-		for _, val := range sw {
-			root := pattern("\\s*[.]\\s+").ReplaceAllString(val, "   none   ")
-			title := pattern("[.]|\\s+").Split(root, 2)[0]
-			unit := pattern("\\s+").Split(pattern("^\\w+\\s*[.]*s*").Split(root, 2)[1], 2)[0]
-			desc := strings.TrimSpace(strings.Split(root, ":")[1])
-			desc = pattern("\\d+\\s*").ReplaceAllString(desc, "")
-			if len(desc) < 1 {
-				desc = "none"
-			}
-			vD := pattern("\\s{2,}\\w*\\s{2,}").Split(strings.Split(root, ":")[0], -1)
-			var value string
-			if len(vD) > 2 && len(vD[len(vD)-1]) > 0 {
-				value = strings.TrimSpace(vD[len(vD)-2])
-			} else {
-				value = strings.TrimSpace(vD[len(vD)-1])
-			}
-			property[title] = WellProps{unit, desc, value}
-		}
-		return property, nil
-	}
-	return
 }
